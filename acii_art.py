b03e83714c6ad71f73319a8af937de0710279433
Anna <anna@MacBook-Air-von-Nutzerin1.local>
2025-06-13 20:29:26 +0200

    new files ascii_art.py & game_logic.py

--- acii_art.py
+++ acii_art.py
@@ -1,2 +1,30 @@
-# List of secret words
-WORDS = ["python", "git", "github", "snowman", "meltdown"]
+
+# Snowman ASCII Art stages
+STAGES = [
+     # Stage 0: Full snowman
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+     """,
+     # Stage 1: Bottom part starts melting
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 2: Only the head remains
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     """,
+     # Stage 3: Snowman completely melted
+     """
+      ___  
+     /___\\ 
+     """
+ ]
